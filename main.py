ba7a30fa2ec353755f620ae0fc6c86aac89520dd
proph-et <andersenfwestphal@gmail.com>
2025-02-16 10:26:56 -0800

    finished, not polished

--- main.py
+++ main.py
@@ -35,7 +35,10 @@ def get_image():
     global filename
     filename = askopenfilename()
     if filename:
+        global image_size
+        global image_colors
         image_size, image_colors = image_array.do_image(filename)
+        create_color_grid()
         print(image_size)
         print(image_colors)
     else:
@@ -86,18 +89,6 @@ def select_color(color):
     selected_color = color
 
 
-solved_states = {}
-
-
-def on_rect_click(event, rect, color, text):
-    if solved_states[rect]:
-        return
-    color_grid.itemconfig(rect, fill=selected_color)
-    if selected_color == color:
-        solved_states[rect] = True
-        color_grid.itemconfig(text, text="")
-
-
 # replace these colors with haddies colors
 colors = {
 }
@@ -120,50 +111,117 @@ i = 0
 for c in new_colors:
     i += 1
     colors[i] = rgb_to_hex(c)
-print(colors)
+colors = {
+    1: '#ffffff',
+    2: '#000000',
+    3: '#ff0000',
+    4: '#00ff00',
+    5: '#0000ff',
+    6: '#ff00ff',
+    7: '#ffff00',
+    8: '#00ffff',
+    9: '#a9a9a9',
+    10: '#ffa500',
+    11: '#ffe0bd',
+    12: '#ffc0cb',
+    13: '#008080'
+}
 
 
 # get the pixel color from the list
 def get_pixel_color(row, col):
-    return colors.get(image_colors[col + row * image_size[0]])
+    index = col + row * image_size[0]
+    color_value = image_colors[index]
+
+    for key, value in colors.items():
+        if color_value == value:
+            # pass
+            return key, color_value
+    #
     # return random.choice(list(colors.items()))
 
 
 # color grid setup
 # need to update these values to be dynamic based on image from haddison
-cols = 96
-rows = 60
-WIDTH = cols * 15
-HEIGHT = (int)(WIDTH / (cols / rows))
-color_grid = Canvas(main, width=WIDTH, height=HEIGHT, bg='black')
-size = int(500 / rows)
-standard_size = size
-for i in range(1, rows + 1):
-    for j in range(1, cols + 1):
-        x1, y1, x2, y2 = (
-            (((WIDTH / cols) * j) - (WIDTH / cols)),
-            (((HEIGHT / rows) * i) - (HEIGHT / rows)),
-            ((WIDTH / cols) * j),
-            ((HEIGHT / rows) * i)
-        )
-        rect_id = color_grid.create_rectangle(x1, y1, x2, y2, fill='#a4bac4', outline="")
+def create_color_grid():
+    solved_states = {}
 
-        center_x = (x1 + x2) / 2
-        center_y = (y1 + y2) / 2
+    def on_rect_click(event, rect, color, text):
+        if solved_states[rect]:
+            return
+        color_grid.itemconfig(rect, fill=selected_color)
+        if selected_color == color:
+            solved_states[rect] = True
+            color_grid.itemconfig(text, text="")
 
-        color_pair = get_pixel_color(i, j)
-        key = color_pair[0]
-        color = color_pair[1]
-        solved_states[rect_id] = False
-        text_id = color_grid.create_text(
-            center_x, center_y,
-            text=f"{key}", font=("Arial", size),
-            fill="black", state="disabled"
-        )
-        color_grid.tag_bind(rect_id, "<Button-1>",
-                            lambda event, r=rect_id, c=color, t=text_id: on_rect_click(event, r, c, t))
+    cols = image_size[0]
+    rows = image_size[1]
+    WIDTH = cols * 30
+    HEIGHT = (int)(WIDTH / (cols / rows))
+    color_grid = Canvas(main, width=WIDTH, height=HEIGHT, bg='black')
+    size = int(500 / rows)
+    standard_size = size
+    for i in range(1, rows):
+        for j in range(1, cols):
+            x1, y1, x2, y2 = (
+                (((WIDTH / cols) * j) - (WIDTH / cols)),
+                (((HEIGHT / rows) * i) - (HEIGHT / rows)),
+                ((WIDTH / cols) * j),
+                ((HEIGHT / rows) * i)
+            )
+            rect_id = color_grid.create_rectangle(x1, y1, x2, y2, fill='#a4bac4', outline="")
 
-color_grid.pack()
+            center_x = (x1 + x2) / 2
+            center_y = (y1 + y2) / 2
+
+            color_pair = get_pixel_color(i, j)
+            key = color_pair[0]
+            color = color_pair[1]
+            solved_states[rect_id] = False
+            text_id = color_grid.create_text(
+                center_x, center_y,
+                text=f"{key}", font=("Arial", size),
+                fill="black", state="disabled"
+            )
+            color_grid.tag_bind(rect_id, "<Button-1>",
+                                lambda event, r=rect_id, c=color, t=text_id: on_rect_click(event, r, c, t))
+            # color_grid.itemconfig(rect_id, fill=color)
+
+    # zoom setup
+    zoom_level = 100
+
+    # def zoom(event):
+    #     global zoom_level
+    #     if zoom_level > 130:
+    #         zoom_level = 130
+    #         amount = 0
+    #     elif zoom_level < 70:
+    #         zoom_level = 70
+    #         amount = 0
+    #     else:
+    #         amount = 0.5 if event.delta < 0 else 1.7
+    #         zoom_level += 10 if event.delta < 0 else -10
+    #     global size
+    #     size = int(zoom_level / standard_size)
+    #     print(zoom_level)
+    #
+    #     def get_all_text_items(canvas):
+    #         for item in canvas.find_all():
+    #             if canvas.type(item) == "text":
+    #                 color_grid.itemconfig(item, font=("Arial", size))
+    #
+    #     get_all_text_items(color_grid)
+    #
+    #     x = main.winfo_pointerx()
+    #     y = main.winfo_pointery()
+    #     abs_coord_x = main.winfo_pointerx() - main.winfo_vrootx()
+    #     abs_coord_y = main.winfo_pointery() - main.winfo_vrooty()
+    #     color_grid.scale(ALL, abs_coord_x, abs_coord_y, amount, amount)
+    #
+    # color_grid.bind('<MouseWheel>', zoom)
+
+    color_grid.pack()
+
 
 # color picker frame setup
 palette_frame = Frame(main)
@@ -182,40 +240,5 @@ for col in range(1, len(colors) + 1):
     button.grid(row=0, column=col, padx=2, pady=2)
     button.config(command=lambda c=colors[col]: select_color(c))
 palette_frame.pack()
-
-# zoom setup
-zoom_level = 100
-
-
-def zoom(event):
-    global zoom_level
-    if zoom_level > 130:
-        zoom_level = 130
-        amount = 0
-    elif zoom_level < 70:
-        zoom_level = 70
-        amount = 0
-    else:
-        amount = 0.5 if event.delta < 0 else 1.7
-        zoom_level += 10 if event.delta < 0 else -10
-    global size
-    size = int(zoom_level / standard_size)
-    print(zoom_level)
-
-    def get_all_text_items(canvas):
-        for item in canvas.find_all():
-            if canvas.type(item) == "text":
-                color_grid.itemconfig(item, font=("Arial", size))
-
-    get_all_text_items(color_grid)
-
-    x = main.winfo_pointerx()
-    y = main.winfo_pointery()
-    abs_coord_x = main.winfo_pointerx() - main.winfo_vrootx()
-    abs_coord_y = main.winfo_pointery() - main.winfo_vrooty()
-    color_grid.scale(ALL, abs_coord_x, abs_coord_y, amount, amount)
-
-
-color_grid.bind('<MouseWheel>', zoom)
 
 main.mainloop()
